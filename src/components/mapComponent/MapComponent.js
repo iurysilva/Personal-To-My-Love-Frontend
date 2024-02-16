c1618fd698951185db16784661e5b6d216d9c4f9
Iuryglabson <iuryglabson18@gmail.com>
2024-02-16 16:42:55 -0300

    feat: adiciona alguns pontos no mapa

--- src/components/mapComponent/MapComponent.js
+++ src/components/mapComponent/MapComponent.js
@@ -11,7 +11,18 @@ const MapComponent = () => {
         -48.491698,
         -48.488995,
         -48.477432,
-        -48.460364
+        -48.460364,
+        -48.479446,
+        -48.48820197704398,
+        -48.47795921720548,
+        -48.472369493892835,
+        -48.48725110460131,
+        -48.48750859665391,
+        -48.47205459565467,
+        -48.45382044602629,
+        -48.44634802513942,
+        -48.46465426501525,
+        -48.46804751639183
     ]
 
     const latitudeLugaresVisitados = [
@@ -19,15 +30,37 @@ const MapComponent = () => {
         -1.508248,
         -1.446021,
         -1.452473,
-        -1.427010
+        -1.427010,
+        -1.388850,
+        -1.4371745048368196,
+        -1.44919390675062,
+        -1.4488962768179456,
+        -1.4373583412493558,
+        -1.4367040880963087,
+        -1.4499421797480159,
+        -1.3561474741693542,
+        -1.3696913319062018,
+        -1.3889987257207463,
+        -1.4143981764724147
     ]
 
     const nomesLugaresVisitados = [
-        "Café com Arte - 27/05/23",
-        "Restaurante da Drika - 17/06/23",
-        "Shopping Boullevard - 19/07/23",
-        "Museu Emílio Goeldi - 22/07/23",
-        "Tacacá do Renato - 26/07/23"
+        "Café com Arte<br>- Onde tudo começou (muito Radiohead, Michael Jackson e Artic Monkeys) - 27/05/23",
+        "Restaurante da Drika<br>- Passeando com meu amor e amigos - 17/06/23",
+        "Shopping Boullevard<br>- Filminho Elementos com meu amor (o mundo quase acaba) - 19/07/23<br>- Lanchando e fazendo Lego - 31/01/24",
+        "Museu Emílio Goeldi<br>- Passeando e vendo os bichinhos no Museu - 22/07/23<br>- Passeando no museu com meu amor e o Samuelzinho - 04/02/24",
+        "Tacacá do Renato<br>- Levando meu amor pra tomar tacacá antes dela viajar - 26/07/23<br>- Novamente tomando tacacá com meu amor - 06/02/24",
+        "Aeroporto Internacional de Val-De-Cans<br>- Chegada do meu amor - 28/01/24",
+        "Ver-O-Açaí<br>- Comemorando nosso aniversário de 6 meses - 01/02/24",
+        "Livraria Travessia<br>- Olhando e comprando alguns livros após o Museu - 22/07/23",
+        "Supermercado Mais Barato<br>- Comendo pizza durante o passeio - 22/07/23",
+        "Praça Brasil<br>- Tomando água de Coco e passeando - 02/02/24",
+        "Café Plenitude<br>- Encontro na cafeteria com meu amor - 02/02/24",
+        "Feira Nippo<br>- Passeando na feirinha com meu amor e o Samuelzinho - 04/02/24",
+        "Instituto Albert Einstein<br>- Recebendo Diploma do meu amor - 05/02/24",
+        "Parque Shopping<br>- Visitando uma nova lojinha no shopping - 05/02/24",
+        "Shopping Grão Pará<br>- Procurando presente pra Dona Rose no shopping - 05/02/24",
+        "Prensadão Lanches<br>- Jantando um lanche gigante com meu amor - 05/02/24"
     ]
 
     useEffect(() => {
